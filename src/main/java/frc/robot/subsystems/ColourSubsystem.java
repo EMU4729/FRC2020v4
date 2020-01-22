030f972fc7915393e3378df012e7614077aaf8bf
LAPTOP <32506926+unswfrc@users.noreply.github.com>
2020-01-22 14:24:17 +1100

    colour sensor works

--- src/main/java/frc/robot/subsystems/ColourSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColourSubsystem.java
@@ -13,27 +13,43 @@ import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
 
 /**
  * Add your docs here.
  */
 public class ColourSubsystem implements Subsystem {
-  private ColorSensorV3 colourSensor;
-
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
 
-  public ColourSubsystem() {
-      colourSensor = new ColorSensorV3(i2cPort);
-  }
-  
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+
+
   public void getColour() {
-      System.out.println(colourSensor.getRed() + ", " + colourSensor.getGreen() + ", " + colourSensor.getBlue());
-    //   return colourSensor.getColour();
+    Color detectedColor = m_colorSensor.getColor();
+
+    String colour = "";
+    double red = detectedColor.red;
+    double green = detectedColor.green;
+    double blue = detectedColor.blue;
+
+
+    if (Math.max(Math.max(red,green),blue) == blue) {
+      colour = "blue";
+    } else if (Math.max(Math.max(red,green),blue) == red) {
+      colour = "red";
+    } else if (Math.max(Math.max(red,green),blue) == green) {
+      if (red > 0.3) {
+        colour = "yellow";
+      } else {
+        colour = "green";
+      }
+    }
+
+    SmartDashboard.putString("Detected Color", colour);
   }
 
   public void initDefaultCommand() {
